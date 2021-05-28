ff599d2592f29ae082f97900949a2d9b54874809
kmrshubham123 <kmrshubham123@gmail.com>
2021-05-28 19:26:16 +0530

    [Kumar Shubham]Added : Fibnocci Series

---
+++ LogicalPrograms/LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    class FibonacciSeries
+    {
+        int n1 = 0;
+        int n2 = 1;
+        int n3;
+        int i;
+        int len;
+        public void Fibbo()
+        {
+            Console.WriteLine("Enter Length");
+            len = int.Parse(Console.ReadLine());
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            
+            for (i = 2; i < len; i++)
+            {
+                n3 = n1 + n2;
+                Console.WriteLine(n3);
+                n1 = n2;
+                n2 = n3;
+            }
+
+    
+        }
+    }
+}
--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -7,6 +7,18 @@ namespace LogicalPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logical Programs");
+            Console.WriteLine("1. Fibonacci Series.");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    FibonacciSeries fibonacciSeries = new FibonacciSeries();
+                    Console.WriteLine("Enter Number:");
+                    int inputFebonacii = Convert.ToInt32(Console.ReadLine());
+                    fibonacciSeries.Fibbo();
+                    break;
+            }
         }
+
     }
 }
