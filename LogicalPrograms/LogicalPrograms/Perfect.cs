277c73117417c61f78cd3555663b2c7cdb28f90b
kmrshubham123 <kmrshubham123@gmail.com>
2021-05-28 21:33:01 +0530

    [Kumar Shubham]Added : Perfect Number

---
+++ LogicalPrograms/LogicalPrograms/Perfect.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    class Perfect
+    {
+
+        public int Find_Factor(int value)
+        {
+            int sum = 0;
+            for (int i = 1; i < value; i++)
+            {
+                if (value % i == 0)
+                    sum = sum + i;
+            }
+            return sum;
+        }
+
+
+    }
+    
+}
--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -7,7 +7,8 @@ namespace LogicalPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logical Programs");
-            Console.WriteLine("1. Fibonacci Series.");
+            Console.WriteLine("1.Fibonacci Series");
+            Console.WriteLine("2.Perfect Number");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
             {
@@ -17,8 +18,27 @@ namespace LogicalPrograms
                     int inputFebonacii = Convert.ToInt32(Console.ReadLine());
                     fibonacciSeries.Fibbo();
                     break;
+                case 2:
+                    //Perfect
+                    Perfect perfect = new Perfect();
+                    Console.WriteLine("Enter number to check is perfect or not:");
+                    int inputPerfect = Convert.ToInt32(Console.ReadLine());
+                    int resultPerfect = perfect.Find_Factor(inputPerfect);
+                    if (inputPerfect == resultPerfect)
+                    {
+                        Console.WriteLine("It is a Perfect Number");
+                    }
+                    else
+                    {
+                        Console.WriteLine("It is not a Perfect Number");
+                    }
+                    break;
+
+
             }
         }
-
     }
 }
+
+                    
+
